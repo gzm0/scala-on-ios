5de245875b42a008cef2c795c6e4fe8dfd8bede3
ericssmith <eric2.71828@gmail.com>
2015-04-07 14:09:16 -0400

    Extract styles

--- src/main/scala/iOSscala/Main.scala
+++ src/main/scala/iOSscala/Main.scala
@@ -8,15 +8,41 @@ object Main extends js.JSApp {
   lazy val View = g.require("View")
   lazy val Text = g.require("Text")
 
+
+
   def main() = {
     val iOSScala = React.createClass(lit(render = () =>
-      React.createElement(View, js.Dynamic.literal(style = js.Dynamic.literal(flex = 1, alignItems = "center", justifyContent = "center",backgroundColor = "#F5FCFF"
-      )),
-        React.createElement(Text, null, "Welcome to React Native!"),
-        React.createElement(Text, null, "To get started, edit Main.scala")
+      React.createElement(View, container,
+        React.createElement(Text, welcome, "Welcome to React Native!"),
+        React.createElement(Text, instructions, "To get started, edit Main.scala")
       )
     ))
 
     React.AppRegistry.registerComponent("iOSscala", () => iOSScala)
   }
+
+  val container = lit(
+    style = lit(
+      flex = 1,
+      alignItems = "center",
+      justifyContent = "center",
+      backgroundColor = "#F5FCFF"
+    ))
+
+  val welcome = lit(
+    style = lit(
+      fontSize = 20,
+      textAlign =  "center",
+      margin = 10
+    )
+  )
+
+  val instructions = lit(
+    style = lit(
+      textAlign = "center",
+      color = "#333333",
+      marginBottom = 5
+    )
+  )
+
 }
